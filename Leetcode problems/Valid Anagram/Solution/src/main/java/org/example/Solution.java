02f44aea4e832462524961f68c682ba4c2525be5
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2024-12-03 08:48:32 -0600

    Add files via upload

---
+++ Leetcode problems/Valid Anagram/Solution/src/main/java/org/example/Solution.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.Arrays;
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+    }
+}
