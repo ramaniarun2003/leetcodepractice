71a881aa1b0ec330d427996a346cf70291767430
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2024-12-03 08:34:59 -0600

    Add files via upload

---
+++ Leetcode problems/Contains Duplicate/Solution/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+    Solution s = new Solution();
+    int [] nums = {1,22,22,3,4};
+    System.out.println(s.hasDuplicate(nums));
+
+
+    }
+}
