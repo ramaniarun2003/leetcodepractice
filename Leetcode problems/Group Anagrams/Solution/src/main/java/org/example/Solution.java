a2f4f39ddeb42144bc65aaa9f13e87806eac1388
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2024-12-23 07:23:50 -0600

    Add files via upload

---
+++ Leetcode problems/Group Anagrams/Solution/src/main/java/org/example/Solution.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            if (visited[i]) continue;
+
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            visited[i] = true;
+
+            for (int j = i + 1; j < strs.length; j++) {
+                if (!visited[j] && isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            result.add(group);
+        }
+
+        return result;
+    }
+
+    private boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        return Arrays.equals(c1, c2);
+    }
+}
