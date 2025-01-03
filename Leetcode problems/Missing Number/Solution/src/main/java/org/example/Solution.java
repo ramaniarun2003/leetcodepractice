22abf4752ecc02b2ed17b428e4f16d0071120140
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2025-01-03 11:19:36 -0600

    Add files via upload

---
+++ Leetcode problems/Missing Number/Solution/src/main/java/org/example/Solution.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
