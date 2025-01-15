b31cd048887fb15a74e6f7f30f17c42fa625d08c
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2025-01-15 15:09:34 -0600

    Add files via upload

---
+++ Leetcode problems/Count Bits/Solution/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.countBits(4)));
+        
+
+    }
+}
