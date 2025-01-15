f8417c5aed0d5fbbd45c99eb7a14b6a98327157a
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2025-01-15 10:02:03 -0600

    Add files via upload

---
+++ Leetcode problems/Valid Parantheses/Solution/src/main/java/org/example/Solution.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Solution {
+    public boolean isValid(String s) {
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+        return s.isEmpty();
+    }
+}
