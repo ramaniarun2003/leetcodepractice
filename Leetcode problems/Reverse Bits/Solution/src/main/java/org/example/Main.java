09cdbb48ae38dd5ce396f8a9274a045c429952bf
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2025-01-16 10:38:38 -0600

    Add files via upload

---
+++ Leetcode problems/Reverse Bits/Solution/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       Solution s = new Solution();
+       int n = 00000000000000000000000000010101;
+       System.out.println(s.reverseBits(n));
+    }
+}
