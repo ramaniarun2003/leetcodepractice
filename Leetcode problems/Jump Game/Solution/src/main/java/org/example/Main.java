9fd33c23712f2618f65bf0d87d308c57c0d2d38d
ramaniarun2003 <134892869+ramaniarun2003@users.noreply.github.com>
2025-01-16 13:07:48 -0600

    Add files via upload

---
+++ Leetcode problems/Jump Game/Solution/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int [] nums = {1,2,0,1,0};
+        System.out.println(s.canJump(nums));
+    }
+}
